ceefef752338142ba37f19287200c3aadd2eb95b
DunevTsvetomir <tzvetomirdunev@gmail.com>
2024-06-05 20:50:09 +0300

    Update: small design improvements and reworks

--- js/model.js
+++ js/model.js
@@ -116,3 +116,20 @@ rightArrow.addEventListener('click', () => requestAnimationFrame(
 leftArrow.addEventListener('click', () => requestAnimationFrame(
 	timestamp => animationFramesSetup(timestamp, distanceInPixels)
 ));
+
+let mybutton = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+mybutton.onclick = function() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
